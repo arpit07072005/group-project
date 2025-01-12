6794cbabf320c88e0bae16ccf9ccc00e620a8fac
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-01-12 12:43:47 +0530

    intial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Page1 from './components/Page1';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Page1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <p>Care<span className="logo">Connect</span></p>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Services</li>
+        <li>Testimonials</li>
+        <li>Contact Us</li>
+
+      </ul>
+      <button className="mainbtn">FindCare</button>
+      <button className="mainbtn">Caregiver</button>
+    </div>
+  )
+}
