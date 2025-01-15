f48d312b140e60a95c8e1b7e4ef10cffcf39a2f1
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-01-15 17:10:50 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
-import React from 'react'
 
+import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 export default function Navbar() {
+  const [menuopen,setMenuopen]=useState(false);
+const handlemenu=()=>{
+  setMenuopen(true);
+}
+const handlemenuclose=()=>{
+  setMenuopen(false);
+}
   return (
+    <>
     <div className="navbar">
       <p>Care<span className="logo">Connect</span></p>
-      <ul className="navli">
+      <ul className="navli remove">
         <li>Home</li>
         <li>About Us</li>
         <li>Services</li>
@@ -14,6 +24,18 @@ export default function Navbar() {
       </ul>
       <button className="mainbtn">FindCare</button>
       <button className="mainbtn">Caregiver</button>
+      <button className="menu"onClick={handlemenu}><MenuIcon/></button>
     </div>
+    <div className={`${menuopen?'togglemenu':'menuclose'}`}>
+    <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Services</li>
+        <li>Testimonials</li>
+        <li>Contact Us</li>
+      </ul>
+      <button className="closemenu"onClick={handlemenuclose}><CloseIcon/></button>
+    </div>
+    </>
   )
 }
